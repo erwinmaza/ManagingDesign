7415719beb4b51559371344fa71e6aee709041af
Erwin <erwinmaza@gmail.com>
2017-12-05 00:49:24 -0600

    Use design config struct

---
+++ ManagingDesign/DesignConstants.swift
@@ -0,0 +1,92 @@
+//  ManagingDesign
+//  Created by Erwin Mazariegos on 12/4/17.
+//  Copyright (c) 2017 eMaza Mobile. All rights reserved.
+
+import UIKit
+
+extension Design {
+
+	enum Colors: String {
+		case accent, text, lightText, clear, white, lightObscuring, error, background, headerBackground
+
+		func uiColor() -> UIColor {
+			guard let color = DesignConfig().colors[self.rawValue] else {
+				fatalError("Specified color not in config: \(self.rawValue)")
+			}
+			return color
+		}
+	}
+
+	enum BackgroundImages: String {
+		case view, header
+
+		func uiImage() -> UIImage {
+			guard let image = DesignConfig().backgroundImages[self.rawValue] else {
+				fatalError("Specified image not in config: \(self.rawValue)")
+			}
+			return image
+		}
+	}
+
+	enum Fonts: String {
+		case light_48, light_36,
+		lightItalic_18, lightItalic_14,
+		italic_15, italic_13,
+		bold_17,
+		regular_16, regular_13
+
+		func uiFont() -> UIFont {
+			guard let font = DesignConfig().fonts[self.rawValue] else {
+				fatalError("Specified font not in config: \(self.rawValue)")
+			}
+			return font
+		}
+	}
+
+	private struct DesignConfig {
+
+		var colors = [String:UIColor]()
+		var backgroundImages = [String:UIImage]()
+		var fonts = [String:UIFont]()
+
+		init() {
+			colors["accent"] = 			UIColor(red:   0/255, green: 181/255, blue: 255/255, alpha: 1)
+			colors["text"] = 			UIColor(red:   0/255, green:   0/255, blue: 100/255, alpha: 1)
+			colors["lightText"] = 		UIColor(red: 193/255, green: 193/255, blue: 255/255, alpha: 0.75)
+			colors["clear"] = 			UIColor(red:   0/255, green:   0/255, blue:   0/255, alpha: 0)
+			colors["white"] = 			UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+			colors["lightObscuring"] = 	UIColor(red:  80/255, green:  80/255, blue:  90/255, alpha: 0.5)
+			colors["error"] = 			UIColor(red: 255/255, green:   0/255, blue:   0/255, alpha: 1)
+			colors["background"] = 		UIColor(patternImage: UIImage(named: "background") ?? UIImage())
+			colors["headerBackground"] = UIColor(patternImage: UIImage(named: "header") ?? UIImage()).withAlphaComponent(0.75)
+
+			backgroundImages["view"] = 	UIImage(named: "background")	?? UIImage()
+			backgroundImages["header"] = UIImage(named: "header")		?? UIImage()
+
+			let substituteFont = UIFont.systemFont(ofSize: 14)
+
+			fonts["light_48"] = 	UIFont(name: "Lato-Light", 	size: 48.0) ?? substituteFont
+			fonts["light_36"] = 	UIFont(name: "Lato-Light", 	size: 36.0) ?? substituteFont
+
+			fonts["lightItalic_18"] = UIFont(name: "Lato-LightItalic", size: 18.0) ?? substituteFont
+			fonts["lightItalic_14"] = UIFont(name: "Lato-LightItalic", size: 14.0) ?? substituteFont
+
+			fonts["italic_15"] = 	UIFont(name: "Lato-Italic", size: 15.0) ?? substituteFont
+			fonts["italic_13"] = 	UIFont(name: "Lato-Italic", size: 13.0) ?? substituteFont
+
+			fonts["bold_17"] = 		UIFont(name: "Lato-Bold", 	size: 17.0) ?? substituteFont
+
+			fonts["regular_16"] = 	UIFont(name: "Lato-Regular", size: 16.0) ?? substituteFont
+			fonts["regular_13"] = 	UIFont(name: "Lato-Regular", size: 13.0) ?? substituteFont
+		}
+
+	}
+
+	private func loadConfig() {
+
+
+	}
+
+}
+
+
